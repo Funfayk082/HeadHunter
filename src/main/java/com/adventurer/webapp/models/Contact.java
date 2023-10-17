f499de945c024d51eef3873adfe77906d2d80920
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2023-10-17 11:29:03 +0300

    Добавил модели для Резюме, включая статусы и контакты

---
+++ src/main/java/com/adventurer/webapp/models/Contact.java
@@ -0,0 +1,21 @@
+package com.adventurer.webapp.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "contact")
+@Data
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contactId;
+
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private ContactType contactType;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "summary_id")
+    private Summary summary;
+}
